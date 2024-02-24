550d2e1dbbfdb3f21e24e014206acc04f47129a0
Evelia Lorena Coss-Navarrete <49405921+EveliaCoss@users.noreply.github.com>
2024-02-24 17:35:26 -0600

    Update qc1.sh

--- Practica_Dia2/scripts/qc1.sh
+++ Practica_Dia2/scripts/qc1.sh
@@ -1,4 +1,4 @@
 # sustituye los paths con los tuyos
-for file in /mnt/Citosina/amedina/ssalazar/claseSTAR/data/*.fastq.gz; do fastqc $file -o /mnt/Citosina/amedina/ssalazar/claseSTAR/quality1; done
-cd /mnt/Citosina/amedina/ssalazar/claseSTAR/quality1
+for file in /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/data/*.fastq.gz; do fastqc $file -o /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/quality1; done
+cd /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/quality1
 multiqc .
