ae65b718bfe4877eb55a0831c3ceef7f55b6d040
Evelia Lorena Coss-Navarrete <49405921+EveliaCoss@users.noreply.github.com>
2024-02-24 17:48:59 -0600

    Update qc2.sh

--- Practica_Dia2/scripts/qc2.sh
+++ Practica_Dia2/scripts/qc2.sh
@@ -1,3 +1,3 @@
-for file in /mnt/Citosina/amedina/ssalazar/claseSTAR/TRIM_results/*.fq.gz; do fast$
-cd /mnt/Citosina/amedina/ssalazar/claseSTAR/quality2
+for file in /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/TRIM_results/*.fq.gz; do fast$
+cd /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/quality2
 multiqc .
