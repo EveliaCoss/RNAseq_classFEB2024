ae2be33f94cd56d18ab60d90a7711499889301dd
Evelia Lorena Coss-Navarrete <49405921+EveliaCoss@users.noreply.github.com>
2024-02-24 21:04:44 -0600

    Update load_data_inR.R

--- Practica_Dia2/scripts/load_data_inR.R
+++ Practica_Dia2/scripts/load_data_inR.R
@@ -1,4 +1,4 @@
-indir <- "/mnt/Citosina/amedina/ssalazar/claseSTAR/STAR_output"
+indir <- "/mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/STAR_output"
 setwd(indir)
 files <- dir(pattern = "ReadsPerGene.out.tab")
 counts <- c() # esta sera la matriz
@@ -7,7 +7,7 @@ for(i in seq_along(files)){
   # as.is para no convertir tipo de datos
   counts <- cbind(counts, x[,2])
 }
-metadata <- read.csv("/mnt/Citosina/amedina/ssalazar/claseSTAR/metadata.csv", header = F)
+metadata <- read.csv("/mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/metadata.csv", header = F)
 colnames(metadata) <- c("sample_id", "type")
 counts <- as.data.frame(counts)
 rownames(counts) <- x[,1]
