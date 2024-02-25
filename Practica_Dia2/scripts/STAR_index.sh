c824e232e946eac20b0222de1c2f2b2c426e59bf
Evelia Lorena Coss-Navarrete <49405921+EveliaCoss@users.noreply.github.com>
2024-02-24 18:38:26 -0600

    Update STAR_index.sh

--- Practica_Dia2/scripts/STAR_index.sh
+++ Practica_Dia2/scripts/STAR_index.sh
@@ -1,6 +1,6 @@
 STAR --runThreadN 30 \
 --runMode genomeGenerate \
---genomeDir /mnt/Citosina/amedina/ssalazar/claseSTAR/STAR_index \
+--genomeDir /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/STAR_index \
 --genomeFastaFiles /mnt/Archives/genome/mouse/mm10/UCSC/chromosomes-1-file/mm10.fa \
---sjdbGTFfile /mnt/Citosina/amedina/ssalazar/claseSTAR/annotation/mm10.knownGene.gtf \
+--sjdbGTFfile /mnt/Guanina/bioinfo24/data/Clase_RNASeq2024/annotation/mm10.knownGene.gtf \
 --sjdbOverhang 99
